dbee26843028ae0d8b9929c1b4eb7d14fe8ec6af
Omenys <105769289+Omenys@users.noreply.github.com>
2022-11-19 14:50:42 -0600

    combat example
    Example of combat mechanic.
    -added player and enemy variables
    -health deduction mechanic until winner determined.

--- Makefile
+++ Makefile
@@ -5,4 +5,5 @@ start:
 	python3 bell-peppers-and-beef/core.py
 
 fix:
+	pip3 install -r requirements.txt
 	autopep8 --recursive --aggressive --in-place bell-peppers-and-beef
--- bell-peppers-and-beef/core.py
+++ bell-peppers-and-beef/core.py
@@ -1,5 +1,18 @@
 def main() -> None:
-    print("Hello World!")
+    player_health = 99
+    enemy_health = 100
+    while player_health > 0 and enemy_health > 0:
+        player_health -=10
+        if player_health <= 0:
+            print("enemy wins!")
+            break
+        print(f"player: {player_health}")
+        enemy_health -=10
+        if enemy_health <= 0:
+            print("player wins!")
+            break
+        print(f"enemy: {enemy_health}")
+    print("combat ended.")
 
 
 if __name__ == "__main__":
