00cea39e191b8ffd4439e10df431589c2773dd5c
Omenys <105769289+Omenys@users.noreply.github.com>
2022-11-19 18:36:31 -0600

    Outline and Combat Edits
    -updated outline ideas for future potential project implementations.
    -added damage randomizer.
    -added player and enemy classes for combat.
    -additional comments for scenarios.

--- bell-peppers-and-beef/core.py
+++ bell-peppers-and-beef/core.py
@@ -1,17 +1,37 @@
+import random
+
+class Entity():
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def get_damage(self):
+        return random.randrange(10, 20)
+
 def main() -> None:
-    player_health = 99
-    enemy_health = 100
-    while player_health > 0 and enemy_health > 0:
-        player_health -=10
-        if player_health <= 0:
-            print("enemy wins!")
+    player = Entity("Player 1", 100)
+    enemy = Entity("Enemy 1", 100)
+
+    while player.hp > 0 and enemy.hp > 0:
+        # Player on defense. Enemy attacking.
+        damage = enemy.get_damage()
+        player.hp -= damage
+        print(f"{player.name} takes {damage} damage.")
+        if player.hp <= 0:
+            print(f"{enemy.name} wins!")
             break
-        print(f"player: {player_health}")
-        enemy_health -=10
-        if enemy_health <= 0:
-            print("player wins!")
+
+        # Enemy on defense. Player attacking.
+        damage = player.get_damage()
+        enemy.hp -= damage
+        print(f"{enemy.name} takes {damage} damage.")
+        if enemy.hp <= 0:
+            print(f"{player.name} wins!")
             break
-        print(f"enemy: {enemy_health}")
+
+        print(f"{player.name}: {player.hp} hp")
+        print(f"{enemy.name}: {enemy.hp} hp\n")
+
     print("combat ended.")
 
 
